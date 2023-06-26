2f1f1fa8bddd3a25b100518a537a1723edec4881
anikjoyy <anikjoy.cse@gmail.com>
2023-06-26 07:25:13 +0600

    add multiple theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Logo from '../assets/logo.svg';
 
 const Header = () => {
+  const [theme, setTheme] = useState(
+    JSON.parse(localStorage.getItem('theme')) || 'medium'
+  );
+
+  useEffect(() => {
+    localStorage.setItem('theme', JSON.stringify(theme));
+    document.documentElement.removeAttribute('class');
+    document.documentElement.classList.add(theme);
+  }, [theme]);
+
   return (
     <header>
       <div className='logo'>
@@ -9,11 +19,38 @@ const Header = () => {
         <span>TaskMate</span>
       </div>
       <div className='themeSelector'>
-        <span className='light'></span>
-        <span className='medium'></span>
-        <span className='gradientOne'></span>
-        <span className='gradientTwo'></span>
-        <span className='gradientThree'></span>
+        <span
+          onClick={() => setTheme('light')}
+          className={theme === 'light' ? 'light activeTheme' : 'light'}
+        ></span>
+        <span
+          onClick={() => setTheme('dark')}
+          className={theme === 'dark' ? 'dark activeTheme' : 'dark'}
+        ></span>
+        <span
+          onClick={() => setTheme('medium')}
+          className={theme === 'medium' ? 'medium activeTheme' : 'medium'}
+        ></span>
+        <span
+          onClick={() => setTheme('gradientOne')}
+          className={
+            theme === 'gradientOne' ? 'gradientOne activeTheme' : 'gradientOne'
+          }
+        ></span>
+        <span
+          onClick={() => setTheme('gradientTwo')}
+          className={
+            theme === 'gradientTwo' ? 'gradientTwo activeTheme' : 'gradientTwo'
+          }
+        ></span>
+        <span
+          onClick={() => setTheme('gradientThree')}
+          className={
+            theme === 'gradientThree'
+              ? 'gradientThree activeTheme'
+              : 'gradientThree'
+          }
+        ></span>
       </div>
     </header>
   );
